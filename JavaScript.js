a3f3f0e25580f00c5c9b1e6f6aa6bd1266c2cd9a
razcrisan <rcrisan13@gmail.com>
2017-11-07 13:27:59 -0500

    added stuff

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,47 @@
-﻿for (var i = 0; i <= 10; i++)
-{
-    console.log(i);
+﻿//console.log("Hello!!");
+//var name = "Razvan";
+//console.log("Hello" + name);
+
+//for (i = 1; i <= 10; i++)
+//{
+//    console.log(i);
+//}
+
+function helloWorld() {
+    var greeting = "Hello World"
+    alert(greeting);
+}
+
+//helloWorld();
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(2, 4));
+
+//This is just a different way of defining functions, just like the above function.
+//var otherFunction = function (a, b) {
+//    return a + b;
+//}
+
+//console.log(otherFunction(2, 8));
+
+//Write a function that takes in a name and prints "Hello [name]."
+//One way
+function userName(name) {
+    console.log("Hello" + name);
+    //return "Hello " + name);
+}
+
+console.log(userName(" Razvan"));
+
+//Other way
+
+
+//this is the updateDiv javascript function used in the html
+
+function updateDiv() {
+    var outputString = "Hello and welcome to Raz's webpage.";
+    document.getElementById("sectionOne").innerHTML = "<p>" + outputString+"</p>"
 }
